905b57100d515ffe3887f2791698c4132504cd3e
scottxiong <github.scott.bear@gmail.com>
2021-09-27 05:17:01 +0800

    fix the bug:"ensure the URL contains embed"

--- api.go
+++ api.go
@@ -2,7 +2,7 @@
 * @Author: scottxiong
 * @Date:   2021-09-27 03:55:47
 * @Last Modified by:   scottxiong
-* @Last Modified time: 2021-09-27 04:35:42
+* @Last Modified time: 2021-09-27 05:16:07
  */
 package youtube
 
@@ -19,6 +19,7 @@ func ShareFrameWithURL(height, width, url string) string {
 	res = strings.Replace(str, "mywidth", width, -1)
 	res = strings.Replace(res, "myheight", height, -1)
 	res = strings.Replace(res, "myurl", url, -1)
+	res = strings.Replace(res, "watch?v=", "embed/", -1)
 	return res
 }
 
